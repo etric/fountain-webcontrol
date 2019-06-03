63547002d7dc8088ded8baa5400ee97b55982d5e
AKhamylov <ackLuxMF16>
2019-06-03 23:37:50 +0300

    Fixed "update system time" logic.

--- src/main/java/com/e3k/fountain/webcontrol/web/WebServer.java
+++ src/main/java/com/e3k/fountain/webcontrol/web/WebServer.java
@@ -47,6 +47,7 @@ public class WebServer {
         get("/api/sysdatetime", (request, response) -> SysDateTimeManager.ONE.getTimeFormatted());
         put("/api/sysdatetime", (request, response) -> {
             SysDateTimeManager.ONE.parseAndUpdateTime(request.body());
+            AlarmClock.ONE.reInitAlarms();
             return "OK";
         });
 
