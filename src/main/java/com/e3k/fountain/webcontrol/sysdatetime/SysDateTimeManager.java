3db62c38a0d070191ac15a65a104fa08889f565e
AKhamylov <ackLuxMF16>
2019-06-05 02:45:04 +0300

    Fix for system date update functionality

--- src/main/java/com/e3k/fountain/webcontrol/sysdatetime/SysDateTimeManager.java
+++ src/main/java/com/e3k/fountain/webcontrol/sysdatetime/SysDateTimeManager.java
@@ -1,6 +1,7 @@
 package com.e3k.fountain.webcontrol.sysdatetime;
 
 import lombok.extern.slf4j.Slf4j;
+import spark.utils.Assert;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
@@ -8,36 +9,47 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
 
 import static java.time.format.DateTimeFormatter.ISO_LOCAL_DATE;
-import static java.time.temporal.ChronoField.*;
+import static java.time.temporal.ChronoField.HOUR_OF_DAY;
+import static java.time.temporal.ChronoField.MINUTE_OF_HOUR;
 
 @Slf4j
 public enum SysDateTimeManager {
 
     ONE;
 
+    //ISO-8601 calendar system -- 2007-12-03T10:15:30
     private static final DateTimeFormatter sysDateCmdPattern =
             new DateTimeFormatterBuilder()
                     .parseCaseInsensitive()
                     .append(ISO_LOCAL_DATE)
                     .appendLiteral(' ')
-//                    .append(ISO_LOCAL_TIME)
                     .appendValue(HOUR_OF_DAY, 2)
                     .appendLiteral(':')
                     .appendValue(MINUTE_OF_HOUR, 2)
                     .toFormatter();
 
     public void parseAndUpdateTime(String localDateTimeRaw) throws IOException, InterruptedException {
-        //ISO-8601 calendar system -- 2007-12-03T10:15:30
-        log.info("Updating System Date & Time: {} --> {}", LocalDateTime.now(), localDateTimeRaw);
-        LocalDateTime.parse(localDateTimeRaw, sysDateCmdPattern); //validation
+        LocalDateTime parsedNewDateTime = LocalDateTime.parse(localDateTimeRaw, sysDateCmdPattern); //validation
 
-        Process process = Runtime.getRuntime().exec("sudo date -s '" + localDateTimeRaw + "'");
+        log.info("Updating System Date & Time: {} --> {}", LocalDateTime.now(), localDateTimeRaw);
+
+        Process process = Runtime.getRuntime().exec(new String[] {"date", "-s", localDateTimeRaw});
         process.waitFor();
 
-        //date -s '2009-02-13 11:31:30' #that's a magical timestamp
+        checkIsEqualToNowUpToMinutes(parsedNewDateTime);
     }
 
     public String getTimeFormatted() {
         return sysDateCmdPattern.format(LocalDateTime.now());
+    }
+
+    private void checkIsEqualToNowUpToMinutes(LocalDateTime dateTime) {
+        LocalDateTime now = LocalDateTime.now();
+        String error = "Date was not updated (correctly)!";
+        Assert.isTrue(now.getYear() == dateTime.getYear(), error);
+        Assert.isTrue(now.getMonth() == dateTime.getMonth(), error);
+        Assert.isTrue(now.getDayOfMonth() == dateTime.getDayOfMonth(), error);
+        Assert.isTrue(now.getHour() == dateTime.getHour(), error);
+        Assert.isTrue(now.getMinute() == dateTime.getMinute(), error);
     }
 }
